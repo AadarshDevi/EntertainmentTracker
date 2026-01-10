38bd4fe829fabd06cecae8a6cece2db2d0357169
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-01-10 15:00:22 -0800

    added single api instancing

--- src/main/java/com/alphagnfss/etr3/api/v1/Api.java
+++ src/main/java/com/alphagnfss/etr3/api/v1/Api.java
@@ -1,4 +1,11 @@
 package com.alphagnfss.etr3.api.v1;
 
 public class Api {
+
+	public static Api api;
+
+	public static Api getInstance() {
+		if (api == null) api = new Api();
+		return api;
+	}
 }
