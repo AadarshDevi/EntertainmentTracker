c572dcd2e3fa4a9df3e781ea3ca953323aab87bd
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-01-10 15:17:12 -0800

    added get methods in api

--- src/main/java/com/alphagnfss/etr3/api/v1/Api.java
+++ src/main/java/com/alphagnfss/etr3/api/v1/Api.java
@@ -1,11 +1,37 @@
 package com.alphagnfss.etr3.api.v1;
 
+import com.alphagnfss.etr3.backend.data.Entertainment;
+import com.alphagnfss.etr3.backend.data.VisualEntertainment;
+import com.alphagnfss.etr3.backend.v1.Backend;
+
 public class Api {
 
 	public static Api api;
+	private final Backend backend;
+
+	public Api() {
+		this.backend = Backend.getInstance();
+	}
 
 	public static Api getInstance() {
 		if (api == null) api = new Api();
 		return api;
 	}
+
+	public Entertainment getEntertainment(int id) {
+		return backend.getEntertainment(id);
+	}
+
+	public VisualEntertainment getVisualEntertainment(int id) {
+		return backend.getVisualEntertainment(id);
+	}
+
+	public Entertainment[] getEntertainments(String text) {
+		return backend.getEntertainments(text);
+	}
+
+	public VisualEntertainment[] getVisualEntertainments(String text) {
+		return backend.getVisualEntertainments(text);
+	}
+
 }
