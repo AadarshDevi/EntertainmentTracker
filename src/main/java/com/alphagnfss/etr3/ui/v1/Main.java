463a476cc7000ca2f16c6ef082872097cb78f730
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-01-10 14:58:14 -0800

    javafx container main enterance class

---
+++ src/main/java/com/alphagnfss/etr3/ui/v1/Main.java
@@ -0,0 +1,12 @@
+package com.alphagnfss.etr3.ui.v1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) throws IOException {
+	}
+}
