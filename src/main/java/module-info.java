185a25db95462dcfe15868c8aa475160582592a0
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-01-10 14:58:29 -0800

    open/export modules for project file

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.alphagnfss.etr3 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.sql;
+	requires org.apache.logging.log4j;
+	requires static lombok;
+	requires com.alphagnfss.etr3;
+
+
+	opens com.alphagnfss.etr3 to javafx.fxml;
+	exports com.alphagnfss.etr3;
+	exports com.alphagnfss.etr3.ui.v1;
+	opens com.alphagnfss.etr3.ui.v1 to javafx.fxml;
+}
