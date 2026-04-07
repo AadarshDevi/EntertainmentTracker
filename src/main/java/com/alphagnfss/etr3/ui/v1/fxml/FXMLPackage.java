e04c55e842c2f2fd3ba09ac65e60fabd2c43c7e4
Aadarsh Devi <aadarsh123456char@gmail.com>
2026-04-06 20:26:05 -0700

    a fxml package to package fxml and its controller

---
+++ src/main/java/com/alphagnfss/etr3/ui/v1/fxml/FXMLPackage.java
@@ -0,0 +1,6 @@
+package com.alphagnfss.etr3.ui.v1.fxml;
+
+import com.alphagnfss.etr3.ui.v1.controllers.Controller;
+import javafx.scene.layout.Pane;
+
+public record FXMLPackage(Pane pane, Controller controller) {}
