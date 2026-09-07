cf6aa87cf07aed16df43be58c838028fab8358e5
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-07 10:57:50 -0700

    Database class implemented DatabaseInterface

--- src/main/java/com/alphagnfss/etr3/database/Database.java
+++ src/main/java/com/alphagnfss/etr3/database/Database.java
@@ -1,4 +1,45 @@
 package com.alphagnfss.etr3.database;
 
+import com.alphagnfss.etr3.communication.Response;
+
 public class Database implements DatabaseInterface {
+    @Override
+    public Response connect() {
+        return null;
+    }
+
+    @Override
+    public Response disconnect() {
+        return null;
+    }
+
+    @Override
+    public Response get(int id) {
+        return null;
+    }
+
+    @Override
+    public Response get(String text) {
+        return null;
+    }
+
+    @Override
+    public Response post(int id) {
+        return null;
+    }
+
+    @Override
+    public Response put(int id) {
+        return null;
+    }
+
+    @Override
+    public Response patch(int id) {
+        return null;
+    }
+
+    @Override
+    public Response delete(int id) {
+        return null;
+    }
 }
