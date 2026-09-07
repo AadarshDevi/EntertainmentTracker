407dabdfb7103cc6b93e8722c8b7e7b4d33ceed0
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-07 10:58:10 -0700

    added fields for the Response

--- src/main/java/com/alphagnfss/etr3/communication/Response.java
+++ src/main/java/com/alphagnfss/etr3/communication/Response.java
@@ -1,4 +1,16 @@
 package com.alphagnfss.etr3.communication;
 
+import lombok.Getter;
+
 public class Response {
+
+    @Getter int code;
+    @Getter String message;
+    @Getter Object object;
+
+    public Response(int code, String message, Object object) {
+        this.code = code;
+        this.message = message;
+        this.object = object;
+    }
 }
