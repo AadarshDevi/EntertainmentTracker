110e1322c2258b7803257d33691754a1ba841479
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-07 10:42:46 -0700

    interface for database

---
+++ src/main/java/com/alphagnfss/etr3/database/DatabaseInterface.java
@@ -0,0 +1,16 @@
+package com.alphagnfss.etr3.database;
+
+import com.alphagnfss.etr3.communication.Response;
+
+import java.net.http.HttpRequest;
+
+public interface DatabaseInterface {
+    Response connect();
+    Response disconnect();
+    Response get(int id);
+    Response get(String text);
+    Response post(int id);
+    Response put(int id);
+    Response patch(int id);
+    Response delete(int id);
+}
