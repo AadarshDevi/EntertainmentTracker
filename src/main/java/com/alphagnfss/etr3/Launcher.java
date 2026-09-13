58f2c6b5cf9aa818814b1bac3bd61c799b9da2ce
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-13 14:23:15 -0700

    created a Movie object

--- src/main/java/com/alphagnfss/etr3/Launcher.java
+++ src/main/java/com/alphagnfss/etr3/Launcher.java
@@ -1,16 +1,19 @@
 package com.alphagnfss.etr3;
 
+import com.alphagnfss.etr3.data.Movie;
 import javafx.application.Application;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Launcher {
 
-	private static final Logger LOGGER = LogManager.getLogger(Launcher.class);
+    private static final Logger LOGGER = LogManager.getLogger(Launcher.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// Set UI
-		Application.launch(Main.class, args);
-	}
+        Movie entertainment = new Movie();
+
+        // Set UI
+        Application.launch(Main.class, args);
+    }
 }
