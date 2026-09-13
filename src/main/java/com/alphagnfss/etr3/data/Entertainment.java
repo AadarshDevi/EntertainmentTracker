1a04663d4d1e986c16f0a47e367dbe6d509aaeb1
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-13 14:23:02 -0700

    added interface methods

--- src/main/java/com/alphagnfss/etr3/data/Entertainment.java
+++ src/main/java/com/alphagnfss/etr3/data/Entertainment.java
@@ -16,4 +16,89 @@ public abstract class Entertainment implements EntertainmentInterface {
     private LocalDate releaseDate;
     private EntertainmentStatus status;
     private EntertainmentType type;
+
+    @Override
+    public boolean getIsFavorite() {
+        return false;
+    }
+
+    @Override
+    public void setIsFavorite(boolean isFavorite) {
+
+    }
+
+    @Override
+    public int getEntertainmentId() {
+        return 0;
+    }
+
+    @Override
+    public void setEntertainmentId(int id) {
+
+    }
+
+    @Override
+    public Vector<Integer> getCollections() {
+        return null;
+    }
+
+    @Override
+    public void setCollections(Vector<Integer> collections) {
+
+    }
+
+    @Override
+    public EntertainmentType getType() {
+        return null;
+    }
+
+    @Override
+    public void setType(EntertainmentType type) {
+
+    }
+
+    @Override
+    public EntertainmentStatus getStatus() {
+        return null;
+    }
+
+    @Override
+    public void setStatus(EntertainmentStatus status) {
+
+    }
+
+    @Override
+    public String getTitle() {
+        return "";
+    }
+
+    @Override
+    public void setTitle(String title) {
+
+    }
+
+    @Override
+    public LocalDate getReleaseDate() {
+        return null;
+    }
+
+    @Override
+    public void setReleaseDate(LocalDate releaseDate) {
+
+    }
+
+    @Override
+    public Vector<String> getTags() {
+        return null;
+    }
+
+    @Override
+    public void setTags(Vector<String> tags) {
+
+    }
+
+    public void print() {
+        System.out.println("\t> Entertainment - Hello World!");
+    }
+
 }
