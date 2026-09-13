f99fa3782e74cdda6156481c72f618fe078b8ec5
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-13 14:23:35 -0700

    data: interface: Segmentable

---
+++ src/main/java/com/alphagnfss/etr3/data/interfaces/Segmentable.java
@@ -0,0 +1,11 @@
+package com.alphagnfss.etr3.data.interfaces;
+
+public interface Segmentable {
+    int getSegmentNumber();
+
+    void setSegmentNumber(int segmentNumber);
+
+    int getPlacementId();
+
+    void setPlacementId(int placementId);
+}
