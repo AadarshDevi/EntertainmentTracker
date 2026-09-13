7c254e4738253eb2b748172d667e554a6c2d242a
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-13 14:04:47 -0700

    data file: abstract level 1: StreamableEntertainment

---
+++ src/main/java/com/alphagnfss/etr3/data/abstracts/StreamableEntertainment.java
@@ -0,0 +1,9 @@
+package com.alphagnfss.etr3.data.abstracts;
+
+import java.util.Vector;
+
+public abstract class StreamableEntertainment {
+    private boolean isSpecial;
+    private boolean isPilot;
+    private Vector<String> productionCompanies;
+}
