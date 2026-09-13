b6aa1ea3e10855463cd8193ab7af072eef11230b
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-13 14:07:15 -0700

    data file: concrete: Episode

---
+++ src/main/java/com/alphagnfss/etr3/data/Episode.java
@@ -0,0 +1,6 @@
+package com.alphagnfss.etr3.data;
+
+import com.alphagnfss.etr3.data.abstracts.StreamableSegmentableEntertainment;
+
+public class Episode extends StreamableSegmentableEntertainment {
+}
