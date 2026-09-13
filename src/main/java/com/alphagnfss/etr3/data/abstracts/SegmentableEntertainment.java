36bbda7fc723e30e2f40a7a972480289608145b7
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-13 14:24:24 -0700

    SegmentableEntertainment implements Segmentable

--- src/main/java/com/alphagnfss/etr3/data/abstracts/SegmentableEntertainment.java
+++ src/main/java/com/alphagnfss/etr3/data/abstracts/SegmentableEntertainment.java
@@ -1,8 +1,9 @@
 package com.alphagnfss.etr3.data.abstracts;
 
 import com.alphagnfss.etr3.data.Entertainment;
+import com.alphagnfss.etr3.data.interfaces.Segmentable;
 
-public abstract class SegmentableEntertainment extends Entertainment {
+public abstract class SegmentableEntertainment extends Entertainment implements Segmentable {
     private int segmentNum;
     private int placementId;
 }
