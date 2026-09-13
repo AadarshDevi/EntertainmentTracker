2387546f5cfc6e8689d9bbd5f81b9979612d7d16
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-13 14:05:46 -0700

    data file: abstract level 2: StreamableListableEntertainment

---
+++ src/main/java/com/alphagnfss/etr3/data/abstracts/StreamableListableEntertainment.java
@@ -0,0 +1,8 @@
+package com.alphagnfss.etr3.data.abstracts;
+
+import java.util.Vector;
+
+public abstract class StreamableListableEntertainment extends StreamableEntertainment {
+    private int segmentCount;
+    private Vector<Integer> segmentList;
+}
