9535f5e28cb9c09cbb14a1298bdf1c40be09c258
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-13 14:04:57 -0700

    data file: abstract level 1: ListableEntertainment

---
+++ src/main/java/com/alphagnfss/etr3/data/abstracts/ListableEntertainment.java
@@ -0,0 +1,8 @@
+package com.alphagnfss.etr3.data.abstracts;
+
+import java.util.Vector;
+
+public abstract class ListableEntertainment {
+    private int segmentCount;
+    private Vector<Integer> segmentList;
+}
