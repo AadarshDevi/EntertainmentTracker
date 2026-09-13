d19e7380a1107b2b53ab8d63c0562e046bf1b341
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-13 14:04:33 -0700

    data file: abstract level 1: SegmentableEntertainment

---
+++ src/main/java/com/alphagnfss/etr3/data/abstracts/SegmentableEntertainment.java
@@ -0,0 +1,8 @@
+package com.alphagnfss.etr3.data.abstracts;
+
+import com.alphagnfss.etr3.data.Entertainment;
+
+public abstract class SegmentableEntertainment extends Entertainment {
+    private int segmentNum;
+    private int placementId;
+}
