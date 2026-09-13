870c0fac8c2905b4f3af30ae4a36bd56aa2e0feb
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-13 14:27:03 -0700

    added missing interface methods to the abstract class

--- src/main/java/com/alphagnfss/etr3/data/abstracts/StreamableSegmentableEntertainment.java
+++ src/main/java/com/alphagnfss/etr3/data/abstracts/StreamableSegmentableEntertainment.java
@@ -5,4 +5,34 @@ import com.alphagnfss.etr3.data.interfaces.Segmentable;
 public abstract class StreamableSegmentableEntertainment extends ViewableStreamableEntertainment implements Segmentable {
     private int segmentNum;
     private int placementId;
+
+    @Override
+    public int getSegmentNumber() {
+        return 0;
+    }
+
+    @Override
+    public void setSegmentNumber(int segmentNumber) {
+
+    }
+
+    @Override
+    public int getPlacementId() {
+        return 0;
+    }
+
+    @Override
+    public void setPlacementId(int placementId) {
+
+    }
+
+    @Override
+    public int getDuration() {
+        return 0;
+    }
+
+    @Override
+    public void setDuration(int duration) {
+
+    }
 }
--- src/main/java/com/alphagnfss/etr3/data/abstracts/ViewableStreamableEntertainment.java
+++ src/main/java/com/alphagnfss/etr3/data/abstracts/ViewableStreamableEntertainment.java
@@ -4,4 +4,14 @@ import com.alphagnfss.etr3.data.interfaces.Viewable;
 
 public abstract class ViewableStreamableEntertainment extends StreamableEntertainment implements Viewable {
     private int duration;
+
+    @Override
+    public int getDuration() {
+        return 0;
+    }
+
+    @Override
+    public void setDuration(int duration) {
+
+    }
 }
