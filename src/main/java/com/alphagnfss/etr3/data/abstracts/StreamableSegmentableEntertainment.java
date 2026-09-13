c14fe12a1b71a36e4f3158ad67a33e5b69e2e407
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-13 14:06:15 -0700

    data file: abstract level 3: StreamableSegmentableEntertainment

---
+++ src/main/java/com/alphagnfss/etr3/data/abstracts/StreamableSegmentableEntertainment.java
@@ -0,0 +1,6 @@
+package com.alphagnfss.etr3.data.abstracts;
+
+public abstract class StreamableSegmentableEntertainment extends ViewableStreamableEntertainment {
+    private int segmentNum;
+    private int placementId;
+}
