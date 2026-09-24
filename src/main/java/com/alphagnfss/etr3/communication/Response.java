286f959cf6a2c7e124be17aa2e315d726607b0f9
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-23 18:12:24 -0700

    added variations of Response constructors that use the base constructor

--- src/main/java/com/alphagnfss/etr3/communication/Response.java
+++ src/main/java/com/alphagnfss/etr3/communication/Response.java
@@ -6,13 +6,29 @@ import lombok.Getter;
 @Builder
 public class Response {
 
-    @Getter int code;
-    @Getter String message;
-    @Getter Object object;
+    @Getter int code; // HTTP Code
+    @Getter String message; // Message (used for exceptions)
+    @Getter Object object; // The object to pass
 
     public Response(int code, String message, Object object) {
         this.code = code;
         this.message = message;
         this.object = object;
+    }
+
+    public Response(int code) {
+        this(code, null, null);
+    }
+
+    public Response(int code, String message) {
+        this(code, message, null);
+    }
+
+    public Response(int code, Object object) {
+        this(code, null, object);
+    }
+
+    public Response notImplemented(String methodHeader) {
+        return new Response(501, "Method not been implemented yet: " + methodHeader + "()", null);
     }
 }
