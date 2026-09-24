4cd1fbd891d0c5021e96667c04cd76bc2472b723
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-23 18:10:59 -0700

    DatabaseAPI implements DatabaseInterface because both use the same methods needed to run the api.

--- src/main/java/com/alphagnfss/etr3/database/DatabaseAPI.java
+++ src/main/java/com/alphagnfss/etr3/database/DatabaseAPI.java
@@ -1,5 +1,59 @@
 package com.alphagnfss.etr3.database;
 
-public class DatabaseAPI {
+import com.alphagnfss.etr3.communication.Response;
+import com.alphagnfss.etr3.data.Entertainment;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.nio.file.Path;
+
+public class DatabaseAPI implements DatabaseInterface {
+
+    @Setter
+    @Getter
+    private DatabaseInterface database;
+
+    public DatabaseAPI(DatabaseInterface database) {
+        this.database = database;
+    }
+
+    @Override
+    public Response createDB(Path path) {
+        return null;
+    }
+
+    @Override
+    public Response deleteDB(Path path) {
+        return null;
+    }
+
+    @Override
+    public Response connect() {
+        return null;
+    }
+
+    @Override
+    public Response disconnect() {
+        return null;
+    }
+
+    @Override
+    public Response createData(Entertainment entertainment) {
+        return null;
+    }
+
+    @Override
+    public Response readData(int id) {
+        return null;
+    }
+
+    @Override
+    public Response updateData(Entertainment entertainment) {
+        return null;
+    }
+
+    @Override
+    public Response deleteData(int id) {
+        return null;
+    }
 }
