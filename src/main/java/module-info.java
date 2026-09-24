e789273adbc7a7cac1eb3bc31be57f584c8f3e95
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-23 18:12:00 -0700

    added nitrite to module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,13 @@
 module com.alphagnfss.etr3 {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires java.sql;
-	requires org.apache.logging.log4j;
-	requires static lombok;
-	requires jdk.compiler;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires org.apache.logging.log4j;
+    requires static lombok;
+    requires jdk.compiler;
     requires jdk.jfr;
+    requires org.dizitart.no2;
+    requires org.dizitart.no2.mvstore;
 
     exports com.alphagnfss.etr3;
 }
