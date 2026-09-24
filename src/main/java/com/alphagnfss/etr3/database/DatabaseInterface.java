d7dcc8ed7b0c6dc2fd0fa5a7ded6df9bb0e59234
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-23 18:11:14 -0700

    added create nd delete db methods

--- src/main/java/com/alphagnfss/etr3/database/DatabaseInterface.java
+++ src/main/java/com/alphagnfss/etr3/database/DatabaseInterface.java
@@ -3,19 +3,28 @@ package com.alphagnfss.etr3.database;
 import com.alphagnfss.etr3.communication.Response;
 import com.alphagnfss.etr3.data.Entertainment;
 
+import java.nio.file.Path;
+
 /**
  * Operations all database classes need to CRUD Operations
  */
 public interface DatabaseInterface {
 
+
+    Response createDB(Path path);
+
+    Response deleteDB(Path path);
+
     /**
      * Connect to a database
+     *
      * @return connection successful
      */
     Response connect();
 
     /**
      * Disconnect from a database
+     *
      * @return disconnection successful
      */
     Response disconnect();
@@ -29,6 +38,7 @@ public interface DatabaseInterface {
 
     /**
      * Read data from the database
+     *
      * @param id
      * @return
      */
