0f0aa5d361254b156214ee86c793274a282e9977
Aadarsh D <aadarsh123456char@gmail.com>
2026-09-25 19:14:13 -0700

    test that will create a database file adn check if it is created

--- src/test/java/com/alphagnfss/etr3/database/DatabaseTest.java
+++ src/test/java/com/alphagnfss/etr3/database/DatabaseTest.java
@@ -1,21 +1,35 @@
 package com.alphagnfss.etr3.database;
 
 import com.alphagnfss.etr3.database.nosql.NitriteDB;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.nio.file.Path;
 
 public class DatabaseTest {
-    private final DatabaseAPI databaseAPI = new DatabaseAPI(new NitriteDB());
+    //    private NitriteDB nitriteDB; // = new NitriteDB();
+    private DatabaseAPI databaseAPI; // = new DatabaseAPI(nitriteDB);
+
+    @BeforeEach
+    public void setup() {
+//        nitriteDB = ;
+        databaseAPI = new DatabaseAPI(new NitriteDB());
+    }
+
+    @AfterEach
+    public void teardown() {
+        databaseAPI.disconnect();
+    }
 
     @Test
     public void createDBTest() {
 
         Assertions.assertEquals(
                 HttpsURLConnection.HTTP_CREATED,
-                databaseAPI.createDB(Path.of("../../../../../../../database.nosql")).getCode()
+                databaseAPI.createDB(Path.of("C:/Users/CryosArtic/IdeaProjects/EntertainmentTracker/src/test/java/com/alphagnfss/etr3/database.db")).getCode()
         );
     }
 
